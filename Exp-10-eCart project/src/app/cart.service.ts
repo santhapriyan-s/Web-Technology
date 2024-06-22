26e9bfb43453566c01f46f589460e512dddaab8e
santhapriyan-s <124667344+santhapriyan-s@users.noreply.github.com>
2024-06-22 21:49:03 +0530

    Add files via upload

---
+++ Exp-10-eCart project/src/app/cart.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  imageUrl: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  private items: Product[] = [];
+
+  constructor() {}
+
+  addToCart(product: Product): void {
+    this.items.push(product);
+  }
+
+  getItems(): Product[] {
+    return this.items;
+  }
+
+  clearCart(): void {
+    this.items = [];
+  }
+}
---
+++ Exp-10-eCart project/src/app/cart/cart.component.ts
@@ -0,0 +1,34 @@
+
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  imageUrl: string;
+}
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent implements OnInit {
+  items: Product[] = [];
+
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.items = this.cartService.getItems();
+  }
+
+  clearCart(): void {
+    this.cartService.clearCart();
+    this.items = [];
+  }
+}
+
+
+
